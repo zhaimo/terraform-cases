540419cedf6f1d239c10378188569d871286f9f9
Haimo Zhang <zhaimo@yahoo.com>
2017-01-24 08:01:25 -0500

    vm

--- main-single-vm.tf
+++ main-single-vm.tf
@@ -1,10 +1,10 @@
 provider "aws" {
   region = "us-east-1"
 }
-resource "aws_instance" "example" {
+resource "aws_instance" "vm" {
   ami = "ami-2d39803a"
   instance_type = "t2.micro"
   tags {
-    Name = "terraform-example"
+    Name = "terraform-vm"
   }
 }
