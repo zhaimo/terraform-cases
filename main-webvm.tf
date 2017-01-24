b4cfa9ee799142a2859004bfc92be7db66ee0906
Haimo Zhang <zhaimo@yahoo.com>
2017-01-24 07:57:25 -0500

    webvm add sg

--- main-webvm.tf
+++ main-webvm.tf
@@ -1,10 +1,22 @@
 provider "aws" {
   region = "us-east-1"
 }
+
+resource "aws_security_group" "instance" {
+  name = "terraform-webvm"
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "webvm" {
   ami = "ami-2d39803a"
   instance_type = "t2.micro"
-  
+  vpc_security_group_ids = ["${aws_security_group.instance.id}"] 
+ 
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
